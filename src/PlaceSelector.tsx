595e8b998e041435f8b1b32b2a0222a9e90ef6c4
Greg Sommerville <35498440+GregSommerville@users.noreply.github.com>
2019-04-24 16:12:42 -0400

    More work on the place selector

--- src/PlaceSelector.tsx
+++ src/PlaceSelector.tsx
@@ -14,9 +14,32 @@ export function PlaceSelector() {
         { place: 'Seattle, WA', lat: 47.606209, lng: -122.332071, tz: -8 }
       ];
 
+      const options = locations.map((loc) => 
+        <option key={loc.place}>{loc.place}</option>
+      );
+
       return (
-        <div className="col-12">
-            PlaceSelector goes here
-        </div>
+        <>
+            <div className="col-3">
+                <b>Presets:</b><br/>
+                <select name='presetSelect' size={10}>
+                {options}
+                </select>
+            </div>
+            <div className='col-3'>
+                <br/>
+                <b>Lat:</b> &nbsp; <input type='text' name='lat'></input><br/><br/>
+                <b>Lng:</b> &nbsp; <input type='text' name='lng'></input><br/><br/>
+                <b>Timezone:</b> &nbsp; 
+                    <select name='timezone'>
+                        <option value='-5'>Eastern</option>
+                        <option value='-6'>Central</option>
+                        <option value='-7'>Mountain</option>
+                        <option value='-8'>Pacific</option>
+                        <option value='-9'>Alaska</option>
+                        <option value='-10'>Hawaii</option>
+                    </select>
+            </div>
+        </>
     );
 }
